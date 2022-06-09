effe472ad4d796a206b8493f64510cb0c7bace1e
matsuyou1001 <matsuyou1001@gmail.com>
2022-06-09 13:49:15 +0900

    ES Modules形式に書き換え

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,45 @@
-const axios = require("axios");
+import axios from 'axios'
 
-exports.exec_webhook = async function (url, avatar_url, username, content, embeds) {
-    try {
-        const payload = {}
-        if (typeof avatar_url === "string") {
-            payload.avatar_url = avatar_url;
+class functions {
+    static async exec_webhook(url, avatar_url, username, content, embeds) {
+        try {
+            const payload = {}
+            if (typeof avatar_url === "string") {
+                payload.avatar_url = avatar_url;
+            }
+            if (typeof username === "string") {
+                payload.username = username;
+            }
+            if (typeof content === "string") {
+                payload.content = content;
+            }
+            if (typeof embeds != "undefined") {
+                payload.embeds = embeds;
+            }
+        
+            await axios.post(
+                url,
+                payload
+            );
         }
-        if (typeof username === "string") {
-            payload.username = username;
-        }
-        if (typeof content === "string") {
-            payload.content = content;
-        }
-        if (typeof embeds != "undefined") {
-            payload.embeds = embeds;
+        catch (error) {
+            console.error(error);
+            throw error;
         }
     
-        await axios.post(
-            url,
-            payload
-        );
-    }
-    catch (error) {
-        console.error(error);
-        throw error;
+        return true;
     }
 
-    return true;
-}
+    static replace_name(name) {
+        switch (name) {
+            case "エイシンフラッシュ": return "**_Eishin Flash_**💖";
+            default: return name;
+        }
+    }
 
-exports.replace_name = function (name) {
-    switch (name) {
-        case "エイシンフラッシュ": return "**_Eishin Flash_**💖";
-        default: return name;
+    static colorcode_to_hex(colorcode) {
+        return parseInt(colorcode.substr(1, 2), 16) * 65536 + parseInt(colorcode.substr(3, 2), 16) * 256 + parseInt(colorcode.substr(5, 2), 16)
     }
 }
 
-exports.colorcode_to_hex = function (colorcode) {
-    return parseInt(colorcode.substr(1, 2), 16) * 65536 + parseInt(colorcode.substr(3, 2), 16) * 256 + parseInt(colorcode.substr(5, 2), 16)
-}
+export default functions;
--- js/main.js
+++ js/main.js
@@ -1,66 +1,68 @@
-const fs = require("fs");
-const func = require("./functions.js");
-const path = require("path");
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+import functions from "./functions.js";
 
-const configPath = path.join(__dirname, "..", "config.json")
-const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
-const webhook_url = config.webhook_url;
-const avatar_url = config.avatar_url;
-const username = config.user_name;
-const thumbnail = config.thumbnail;
-const color = config.color;
-const fields_count = typeof config.fields_count === "undefined" ? 2 : Number(config.fields_count);
+async function main() {      
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-const dataPath = path.join(__dirname, "..", "data.json")
-const data = JSON.parse(fs.readFileSync(dataPath, "utf8"));
-const now = new Date();
-const today = ("0" + (now.getMonth() + 1)).slice(-2) + "/" + ("0" + (now.getDate())).slice(-2);
+    const configPath = path.join(__dirname, "..", "config.json")
+    const { webhook_url, avatar_url, user_name, thumbnail, color, fields_count } = JSON.parse(await fs.readFile(configPath, "utf8"));
 
-const todayumas = data.umamusumes.filter(uma => uma.birthday == today);
-if (todayumas.length == 0) {
-    process.exit(1);
-}
+    const dataPath = path.join(__dirname, "..", "data.json")
+    const { umamusumes } = JSON.parse(await fs.readFile(dataPath, "utf8"));
+    const now = new Date();
+    const today = ("0" + (now.getMonth() + 1)).slice(-2) + "/" + ("0" + (now.getDate())).slice(-2);
 
-const title = `🎉 今日は、${todayumas.map(uma => func.replace_name(uma.name)).join("、")}の誕生日です！`
-
-let current = today, elapsedYears = 0;
-const fields = [];
-for (let i = 1; i <= fields_count; i++ ) {
-    const isLast = !data.umamusumes.some(uma => current < uma.birthday);
-    if (isLast) elapsedYears++;
-
-    const nextday = isLast ?
-        data.umamusumes.map(uma => uma.birthday).sort().find(() => true) :
-        data.umamusumes.map(uma => uma.birthday).sort().find(day => current < day) ;
-
-    const nextumas = data.umamusumes.filter(uma => uma.birthday == nextday)
-    const date = new Date(`${now.getFullYear() + elapsedYears}/${nextday}`)
-    const days = Math.ceil((date - now) / 86400000);
-
-    fields.push({
-        name: `${date.getMonth() + 1}/${date.getDate()} (${days == 1 ? "明日" : days + "日後"})`,
-        value: nextumas.map(uma => uma.name).join("、")
-    });
-
-    current = nextday;
-}
-
-const embeds = [
-    {
-        title: title,
-        fields: fields,
+    const todayumas = umamusumes.filter(uma => uma.birthday == today);
+    if (todayumas.length == 0) {
+        return 1;
     }
-];
 
-if (typeof thumbnail === "string") {
-    embeds[0].thumbnail = {
-        url: thumbnail
+    const title = `🎉 今日は、${todayumas.map(uma => functions.replace_name(uma.name)).join("、")}の誕生日です！`
+
+    let current = today, elapsedYears = 0;
+    const fields = [];
+    for (let i = 1; i <= fields_count; i++ ) {
+        const isLast = !umamusumes.some(uma => current < uma.birthday);
+        if (isLast) elapsedYears++;
+
+        const nextday = isLast ?
+            umamusumes.map(uma => uma.birthday).sort().find(() => true) :
+            umamusumes.map(uma => uma.birthday).sort().find(day => current < day) ;
+
+        const nextumas = umamusumes.filter(uma => uma.birthday == nextday)
+        const date = new Date(`${now.getFullYear() + elapsedYears}/${nextday}`)
+        const days = Math.ceil((date - now) / 86400000);
+
+        fields.push({
+            name: `${date.getMonth() + 1}/${date.getDate()} (${days == 1 ? "明日" : days + "日後"})`,
+            value: nextumas.map(uma => uma.name).join("、")
+        });
+
+        current = nextday;
     }
+
+    const embeds = [
+        {
+            title: title,
+            fields: fields,
+        }
+    ];
+
+    if (typeof thumbnail === "string") {
+        embeds[0].thumbnail = {
+            url: thumbnail
+        }
+    }
+
+    if (typeof color === "string") {
+        embeds[0].color = functions.colorcode_to_hex(color);
+    }
+
+    const successed = await functions.exec_webhook(webhook_url, avatar_url, user_name, null, embeds);
+    return successed ? 0 : 1;
 }
 
-if (typeof color === "string") {
-    embeds[0].color = func.colorcode_to_hex(color);
-}
-
-func.exec_webhook(webhook_url, avatar_url, username, null, embeds)
-    .catch(() => process.exit(1));
+const exitCode = await main();
+process.exit(exitCode);
